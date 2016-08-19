8e21cf41b68d7ef3837585aa4f6869bf2f075612
martinski74 <martin.dobr@abv.bg>
2016-08-19 11:15:42 +0300

    up-date

--- MyHealthyBlog/MyHealthyBlog/Migrations/Configuration.cs
+++ MyHealthyBlog/MyHealthyBlog/Migrations/Configuration.cs
@@ -26,65 +26,35 @@ namespace MyHealthyBlog.Migrations
                 CreateUser(context, "admin@gmail.com", "123", "System Administrator");
                 CreateUser(context, "pesho@gmail.com", "123", "Peter Ivanov");
                 CreateUser(context, "merry@gmail.com", "123", "Maria Petrova");
-                CreateUser(context, "geshu@gmail.com", "123", "George Petrov");
 
                 CreateRole(context, "Administrators");
                 AddUserToRole(context, "admin@gmail.com", "Administrators");
 
                 CreatePost(context,
-                    title: "Work Begins on HTML5.1",
-                    body: @"<p>The World Wide Web Consortium (W3C) has begun work on <b>HTML5.1</b>, and this time it is handling the creation of the standard a little differently. The specification has its <b><a href=""https://w3c.github.io/html/"">own GitHub project</a></b> where anyone can see what is happening and propose changes.</p>
-                    <p>The organization says the goal for the new specification is ""to <b>match reality better</b>, to make the specification as clear as possible to readers, and of course to make it possible for all stakeholders to propose improvements, and understand what makes changes to HTML successful.""</p>
-                    <p>Creating HTML5 took years, but W3C hopes using GitHub will speed up the process this time around. It plans to release a candidate recommendation for HTML5.1 by <b>June</b> and a full recommendation in <b>September</b>.</p>",
+                    title: "Подсладителите",
+                    body: @"<p>Навярно много от нашите читатели са главно запознати с вредите от захарта и най-често се замислят първо за фигурата си при покупката на сладки продукти. Всъщност по- малко калоричните й заместители са истинските вредители. Най-често съобщението „без захар“ върху опаковките на любимите ви сладки изделия означават, че захарта е заменена с аспартам. Аспартамът е изкуствен подсладител, създаден в лабораториите на “G.D. Searle” през 1965. Научните изследвания в лабораторията на фармацевтичната компания откриват </p>
+                    <p>След одобрение от Американската асоциация по храните и лекарствата през 1974 той постепенно започва да навлиза в хранителната индустрия, като се популяризира изключително бързо. Въпреки това фармацевтичният гигант “G.D. Searle” бива разследван от екип от специалисти, заради </p>
+                    ",
                     date: new DateTime(2016, 03, 27, 17, 53, 48),
                     authorUsername: "merry@gmail.com"
                 );
 
                 CreatePost(context,
-                    title: "Windows 10 Preview with Bash Support Now Available",
-                    body: @"<p>Microsoft has released a new <b>Windows 10 Insider Preview</b> that includes native support for <b>Bash running on Ubuntu Linux</b>. The company first announced the new feature at last week''s Build development conference, and it was one of the biggest stories of the event. The current process for installing Bash is a little complication, but Microsoft has a blog post that explains how the process works.</p>
-                    <p>The preview build also includes <b>Cortana</b> upgrades, extensions support, the new <b>Skype</b> Universal Windows Platform app and some interface improvements.</p>",
+                    title: "Температура на хранене",
+                    body: @"<p>Ензимите са жизненоважни за човешкия организъм. Незаменими са при смилането на храната и дават възможност на веществата в нея да бъдат абсорбирани в кръвта и от там да се използват при различните функции на тялото. Ензимите са основните виновници за доставянето на енергия на организма, за възстановяването на клетките, тъканите и органите, затова д-р Едуард Хауъл, лекар и пионер в ензимните изследвания, ги нарича „искрици живот”. Изключително важен фактор за ефективността на храносмилателните ензими се оказва температурата. Днешната храна като цяло е бедна на ензими, тъй като на трапезата си сервираме предимно готвени, термично обработени ястия и все по-малко пресни плодове и зеленчуци.</p>
+                    <p>При ниска температура на храната (под 10°C) ензимите не функционират. Поради това е нужно студени и замразени храни да не се консумират директно, а да престоят на стайна температура. При загряване над 40°C ефективността на ензимите започва да намалява, а при 70°C действието им напълно замира. Затова е необходим техният допълнителен прием, за да може да се възстанови балансът в организма.</p>",
                     date: new DateTime(2016, 05, 11, 08, 22, 03),
                     authorUsername: "merry@gmail.com"
                 );
 
                 CreatePost(context,
-                    title: "Atom Text Editor Gets New Windows Features",
-                    body: @"<p>GitHub has released <b>Atom 1.7</b>, and the updated version of the text editor offers improvements for Windows developers. Specifically, it is now easier to build in Visual Studio, and it now supports the Appveyor CI continuous integration service for Windows.</p>
-                    <p>Other new features include improved tab switching, tree view and crash recovery. GitHub noted, ""Crashes are nobody''s idea of fun, but in case Atom does crash on you, it periodically saves your editor state. After relaunching Atom after a crash, you should find all your work saved and ready to go.""</p>
-                    <p>GitHub has also released a beta preview of Atom 1.8.</p>",
+                    title: "Класификация на компонентите в храните и добавките",
+                    body: @"<p>Терминологията в това отношение сега е доста объркана, като за дадено понятие на различните места има различни пояснения. Тук бихме искали да предложим едно сравнително по-ясно подразделяне.
+•  Подправките са от растителен или минерален, по-рядко от синтетичен произход, и преди всичко се използват за постигане на вкусов ефект, макар че някои имат и консервиращи свойства. Влиянието им върху здравето е нееднозначно; между тях има и прекрасни билки, соли и органични киселини, но при предозиране дори и те могат да доведат до неблагоприятни резултати.</p>
+                    <p> Добавките  – в случая става дума за биологичноактивните добавки (БАД) – те са с концентрирано съдържание на хранителни вещества или биопротектори (антиоксиданти, билкови екстракти). Могат да са от природен, полусинтетичен или синтетичен произход (вкл. аминокиселини, витамини и минерали). Обикновено в малки дози са полезни и без странични ефекти, макар че например в т.нар. ортомолекулярна медицина отделни субстанции се прилагат безвредно и в големи количества, но при съответни условия. Над определена граница обаче добавките могат да са добри само за определени индивиди, докато на други да навредят. Следователно, при употребата им във високи дози е необходимо индивидуално съобразяване! Важно е опаковките да бъдат придружени от информация за състава, срока на годност и евентуалните противопоказания.</p>
+                    ",
                     date: new DateTime(2016, 03, 27, 17, 53, 48),
                     authorUsername: "merry@gmail.com"
-                );
-
-                CreatePost(context,
-                    title: "SoftUni 3.0 Just Launched",
-                    body: @"<p>The <b>Software University (SoftUni)</b> launched a new training methodology and training program for software engineers in Sofia.</p>
-                    <p>It is a big step ahead. Now SoftUni offers several professions:</p>
-                    <ul>
-                      <li>PHP Developer</li>
-                      <li>JavaScript Developer</li>
-                      <li>C# Web Developer</li>
-                      <li>Java Web Developer</li>
-                    </ul>",
-                    date: new DateTime(2016, 02, 18, 22, 14, 38),
-                    authorUsername: "pesho@gmail.com"
-                );
-
-                CreatePost(context,
-                    title: "Git 2.8 Adds Security and Productivity Features",
-                    body: @"<p>Version 2.8 of the open-source distributed version-control system Git has been released. The new edition provides a variety of new features, bugfixes and other improvements.</p>
-                    <p>According to GitHub, the most notable new features include:</p>
-                    <ul>
-                    <li><strong>Parallel fetches of submodules:</strong> “Using ‘git submodules,’ one Git repository can include other Git repositories as subdirectories. This can be a useful way to include libraries or other external dependencies into your main project. The top-level repository specifies which submodules it wants to include, and which version of each submodule,” wrote Jeff King, a Git team member, in a <a href=""https://github.com/blog/2131-git-2-8-has-been-released"">blog post</a>. According to him, if users have multiple submodules, fetches can be time-consuming. The latest release allows users to fetch from multiple submodules in parallel.</li>
-                    <li><strong>Don’t guess my identity: </strong>Instead of using one e-mail address for all of a user’s open-source projects, they can now tell Git what user name and e-mail they want to use before they commit.</li>
-                    <li><strong>Convergences with Git for Windows:</strong> The Git team has been working on making Git as easy to work with on Windows as it is on Linux and OS X. The latest release includes Git commands rewritten in C; Windows-specific changes from the Git for Windows project; and the ability to accept both LF and CRLF line endings. “This continuing effort will make it easier to keep the functionality of Git in sync across platforms as new features are added,” King wrote.</li>
-                    <li><strong>Security fixes: </strong>Git 2.8 addresses the vulnerability CVE-2016-2324. There have not been any reported exploits, but the vulnerability could execute arbitrary code when cloning a malicious repository, according to King.</li>
-                    </ul>
-                    <p>Other features include the ability to turn off Git’s clean and smudge filters; the ability to see where a particular setting came from; the ability to easily diagnose end-of-line problems; the ability to see a remote repository’s default branch; and support for cloning via the rsync protocol has been dropped.</p>
-                    <p>The full release notes are available <a href=""https://github.com/git/git/blob/v2.8.0/Documentation/RelNotes/2.8.0.txt"">here</a>.</p>",
-                    date: new DateTime(2016, 04, 11, 19, 02, 05),
-                    authorUsername: "geshu@gmail.com"
                 );
 
                 CreatePost(context,
